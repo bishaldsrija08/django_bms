daee2ac8ea41f4354605465677407720c217575d
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-06-07 17:17:55 +0545

    login

--- myproject/blog/views/auth_view.py
+++ myproject/blog/views/auth_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
 def render_register_form(request):
     if request.method == "POST":
@@ -11,8 +12,7 @@ def render_register_form(request):
         user = User.objects.create(username=username, email=email)
         user.set_password(password)
         user.save()
-
-        # return HttpResponse("Form submitted successfully!")
+        messages.success(request, "Account created successfully")
         return redirect("/blog/login")
     else:
         return render(request, "auth/register.html")
@@ -20,7 +20,15 @@ def render_register_form(request):
 
 def render_login_form(request):
     if request.method == "POST":
-        # Handle form submission and user login logic here
-        print("Form submitted")
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            messages.success(request, "Login successful")
+            return redirect("/blog/")
+        else:
+            messages.error(request, "Invalid email or password")
+            return render(request, "auth/login.html")
+        
     else:
         return render(request, "auth/login.html")
